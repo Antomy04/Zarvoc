3b360694818698738ba110daa7cc69bee7c2a4b5
KK <Kksingh2228@gmail.com>
2025-08-03 14:02:36 +0000

    comment

--- Frontend/src/UserComponents/CategoryPage.jsx
+++ Frontend/src/UserComponents/CategoryPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Category from './Category';
-import './CategoryPage.css';
+// import './CategoryPage.css';
 
 const CategoryPage = () => {
   return (
